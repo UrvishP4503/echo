beff0d74d9e4a3b4a5095e0df6265c4d64c3e20f
Urvish <urvishp45003@gmail.com>
2023-02-28 17:56:49 +0530

    hi

--- basics/math.sh
+++ basics/math.sh
@@ -1,13 +1,20 @@
-num1 = 1
-num2 = 5
+#!/bin/bash
+let num1=1
+let num2=5
 
-let num2 += 5
+let num2+=5
 
 echo $num1
 echo $num2
 
-num3 = $((num1 + num2)) 
-# also we can do: let num3 = num1 + num2
+num3=$((num1 + num2)) 
+# also we can do: let num3=num1+num2
+
+echo $((2**2)) # this is 2 to the power 2
+echo $((2*2)) 
+echo $((2+2)) 
+echo $((2-2)) 
+echo $((2/2)) 
 
 cat<< END
 this is how
